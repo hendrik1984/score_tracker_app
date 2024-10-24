02f40dd2c6668a6d20f921731ce4c87abd3f94a7
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-10-24 17:44:34 +0700

    fix admin dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  authenticated :user do
-    get 'admin/dashboard', to: 'admin#dashboard', as: :admin_dashboard
-  end
+  get 'admin/dashboard', to: 'admin#dashboard', as: :admin_dashboard
 
   root to: 'points#index'
 end
